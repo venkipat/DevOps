e3378cf2d3d79833d3cb2d24b7e444f5b8a4d9b2
venkatesh <venki.pat@gmail.com>
2017-04-08 13:42:17 +0530

    first commit

---
+++ src/main/java/com/devops/DevOpsApplication.java
@@ -0,0 +1,12 @@
+package com.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsApplication.class, args);
+	}
+}
