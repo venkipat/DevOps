b2555f75740943c6ce7ce81b31b132d9c03ac24a
venkatesh <venki.pat@gmail.com>
2017-04-09 00:14:27 +0530

    tomcat compatible application

--- src/main/java/com/devops/DevOpsApplication.java
+++ src/main/java/com/devops/DevOpsApplication.java
@@ -2,10 +2,17 @@ package com.devops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class DevOpsApplication {
+public class DevOpsApplication extends SpringBootServletInitializer {
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DevOpsApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DevOpsApplication.class, args);
 	}
